d78bc47d3b37679c1cb96da6fb046d8440edefe7
Hanbee Lee <bee1162@naver.com>
2023-02-12 23:04:30 +0900

    feat:api/uploadOnDevice #1

--- api/index.js
+++ api/index.js
@@ -6,12 +6,16 @@ const User = require('./models/User.js');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const imageDownloader = require('image-downloader');
+const multer = require('multer');
+const fs = require('fs');
 
 require('dotenv').config();
 
 //parsing json
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname + '/uploads'));
 const bcryptSalt = bcrypt.genSaltSync(10);
 const jwtSecret = 'sdafdafsdfdassdfa';
 
@@ -80,6 +84,32 @@ app.get('/profile', (req, res) => {
 
 app.post('/logout', (req, res) => {
   res.cookie('token', '').json(true);
+});
+
+console.log({ __dirname });
+app.post('/upload-by-link', async (req, res) => {
+  const { link } = req.body;
+  const newName = 'photo' + Date.now() + '.jpg';
+  await imageDownloader.image({
+    url: link,
+    dest: __dirname + '/uploads/' + newName,
+  });
+  res.json(newName);
+});
+
+const photosMiddleware = multer({ dest: 'uploads/' });
+app.post('/upload', photosMiddleware.array('photos', 100), (req, res) => {
+  console.log(req.files);
+  const uploadedFiles = [];
+  for (let i = 0; i < req.files.length; i++) {
+    const { path, originalname } = req.files[i];
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    const newPath = path + '.' + ext;
+    fs.renameSync(path, newPath);
+    uploadedFiles.push(newPath.replace('uploads\\', ''));
+  }
+  res.json(uploadedFiles);
 });
 app.listen(4000);
 
